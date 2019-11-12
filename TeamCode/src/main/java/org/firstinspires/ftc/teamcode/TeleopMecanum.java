9c593038d2e066b1a4fc890b6cd2c1ae8a4fceca
Zenos5 <AngelJW@outlook.com>
2019-11-12 15:12:12 -0800

    everything i hope

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMecanum.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//Define Code Name for phone
+//Define Code Name for phon
 @TeleOp(name="TeleOpMecanum")
 public class TeleopMecanum extends OpMode
 {
